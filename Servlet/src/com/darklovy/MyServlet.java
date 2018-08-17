a8967962d6c762cdcc55759feeeff2e1ed256a23
ttxxly <ttxxly@gmail.com>
2018-08-17 17:38:18 +0800

    add Servlet module.

---
+++ Servlet/src/com/darklovy/MyServlet.java
@@ -0,0 +1,34 @@
+package com.darklovy;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+
+    /**
+     * 处理用户提交的 Get 请求
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    /**
+     * 处理用户提交的 Post 请求
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
